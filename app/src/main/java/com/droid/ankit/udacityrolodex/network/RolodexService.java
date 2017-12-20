534f7609a0011c74da7352d55a0badb7609bd9a8
ankit <ankit.v.shetgaonkar@gmail.com>
2017-12-20 15:51:11 -0600

    initial commit

---
+++ app/src/main/java/com/droid/ankit/udacityrolodex/network/RolodexService.java
@@ -0,0 +1,10 @@
+package com.droid.ankit.udacityrolodex.network;
+
+/**
+ * Created by Ankit on 12/20/2017.
+ */
+
+public interface RolodexService {
+//     @GET("users/{user}/repos")
+//     Call<List<Repo>> listRepos(@Path("user") String user);
+}
